3595c845fb2acfe11bb45b9519e9bf02f1ab8a35
OliverEC <oliverec04@gmail.com>
2021-04-12 10:04:41 +0200

    lort

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("hello world!")
+# Pins
+STRIPPINS = [7, 8, 2, 3, 4]
+BTNPIN = 6
+LEDPIN = 5
+BUZZPIN = 16
+PRESS1PIN = 0
+PRESS2PIN = 1
+
+# Constants
+STRIPCOUNT = 30
+MOVESPEED = 5
+
+# Variables
+tick = 0
+hitTick = 0
+track = []
+
+print("hej")
+
+for i in range(5):
+    for j in range(STRIPCOUNT):
+        track[i][j] = 0
+
+print(track)
