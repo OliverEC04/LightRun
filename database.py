c1d937a21a70e11c5705158eb8942537505b3d64
OliverEC04 <oliverec04@gmail.com>
2021-05-12 13:23:45 +0200

    data

--- database.py
+++ database.py
@@ -1,23 +1,34 @@
 import sqlite3
 
-try:
-    sqliteConnection = sqlite3.connect('database.db')
-    cursor = sqliteConnection.cursor()
-    print("Successfully Connected to SQLite")
+class Database:
+    def __init__(self, path):
+        self.path = path
 
-    sqlite_insert_query = """INSERT INTO Scoreboard
-                          (name, score, date) 
-                           VALUES 
-                          ('william','1000','404731040741807')"""
+    def insertScore(self, user, score):
+        status = "waiting"
+        date = 1
 
-    count = cursor.execute(sqlite_insert_query)
-    sqliteConnection.commit()
-    print("Record inserted successfully into Scoreboard table", cursor.rowcount)
-    cursor.close()
+        try:
+            sqliteConnection = sqlite3.connect(self.path)
+            cursor = sqliteConnection.cursor()
+            print("Successfully Connected to SQLite")
 
-except sqlite3.Error as error:
-    print("Failed to insert data into sqlite table", error)
-finally:
-    if sqliteConnection:
-        sqliteConnection.close()
-        print("The SQLite connection is closed")
+            sqlite_insert_query = """INSERT INTO Scoreboard
+                                (name, score, date) 
+                                VALUES 
+                                ('{0}','{1}','{2}')""".format(user, score, date)
+
+            count = cursor.execute(sqlite_insert_query)
+            sqliteConnection.commit()
+            print("Record inserted successfully into Scoreboard table", cursor.rowcount)
+            cursor.close()
+
+        except sqlite3.Error as error:
+            print("Failed to insert data into sqlite table", error)
+        finally:
+            if sqliteConnection:
+                sqliteConnection.close()
+                print("The SQLite connection is closed")
+
+database = Database("database.db")
+database.insertScore("Neger", 10)
