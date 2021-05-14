3c840d15779439f0b7c5b9e1e907ccd1111b8815
OliverEC <oliverec04@gmail.com>
2021-05-15 00:38:27 +0200

    led

--- scripts/main.py
+++ scripts/main.py
@@ -46,6 +46,7 @@ class Strip:
         if (self.tick % self.tileHeight == 0):
             for x in range(self.size.x):
                 for y in range(self.size.y):
+                    print(self.track[x][y + 1])
                     self.track[x][y] = self.track[x][y + 1]
 
         trackIndex = 0
