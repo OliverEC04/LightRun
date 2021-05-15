330a98d6881a300ed5f1531cc672c00c3da39aef
OliverEC <oliverec04@gmail.com>
2021-05-15 23:38:19 +0200

    commit

--- scripts/main.py
+++ scripts/main.py
@@ -72,6 +72,7 @@ class Strip:
                     else:
                         stripIndex = math.floor(self.posToIndex(Vector2(x, y)) * self.tileHeight + i)
 
+                    print(stripIndex)
                     self.led.setPixelColor(stripIndex, self.track[x][y].value)
 
         # trackIndex = 0
