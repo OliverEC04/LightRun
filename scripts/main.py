d629ece825407663c65cdfbd53d1b2bb5c37a052
OliverEC <oliverec04@gmail.com>
2021-05-16 19:40:44 +0200

    commit

--- scripts/main.py
+++ scripts/main.py
@@ -4,7 +4,10 @@ import sqlite3
 import math
 from enum import Enum
 from grovepi import *
-import RPi.GPIO as GPIO
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 from rpi_ws281x import *
 
 """
