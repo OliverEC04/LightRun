148dc3aa7a790bc3ea2771ac31ee38ca36121da4
OliverEC <oliverec04@gmail.com>
2021-05-17 21:38:07 +0200

    com

--- scripts/main.py
+++ scripts/main.py
@@ -342,7 +342,7 @@ layout = [
     ]
 ]
 
-window = sg.Window("LightRun", layout).Finalize()
+window = sg.Window("LightRun", layout, no_titlebar=True, location=(0,0), size=(800,600), keep_on_top=True).Finalize()
 window.Maximize()
 
 # strip.queueSegment(segments[0])
