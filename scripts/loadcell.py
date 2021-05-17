bf50c2cf1662d7889db3084785c9f635fa023960
OliverEC <oliverec04@gmail.com>
2021-05-17 15:14:30 +0200

    hI

--- scripts/loadcell.py
+++ scripts/loadcell.py
@@ -5,7 +5,7 @@ from loadcell_class import HX711  # import the class HX711
 
 while True:
     GPIO.setmode(GPIO.BCM)  # set GPIO pin mode to BCM numbering
-    hx = HX711(dout_pin=11, pd_sck_pin=13)  # create an object
+    hx = HX711(dout_pin=13, pd_sck_pin=11)  # create an object
     print(hx._read())  # get raw data reading from hx711
     GPIO.cleanup()
 
