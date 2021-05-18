280fbff603e8b0689c8e00ae1867eb14b2996062
OliverEC <oliverec04@gmail.com>
2021-05-18 09:51:25 +0200

    hej

--- scripts/main.py
+++ scripts/main.py
@@ -401,9 +401,9 @@ layout = [
     ]
 ]
 
-window = sg.Window("LightRun", layout, no_titlebar=False, location=(50,50), size=(1800,900), keep_on_top=True).Finalize()
-# window.Maximize()
-event, values = window.read()
+# window = sg.Window("LightRun", layout, no_titlebar=False, location=(50,50), size=(1800,900), keep_on_top=True).Finalize()
+# # window.Maximize()
+# event, values = window.read()
 updateScoreboard()
 
 # strip.queueSegment(segments[0])
